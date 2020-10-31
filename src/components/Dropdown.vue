e17714d0238095f6cccbf177c97659c6e7af2c30
GarenWang <2270099149@qq.com>
2020-10-31 08:55:25 +0800

    feat: 添加dropdown组件

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="dropdown">
+    <a class="btn btn-outline-light my-2 dropdown-toggle" href="#" @click.prevent="toggleOpen">
+      {{title}}
+    </a>
+    <div class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
+      <li class="dropdown-item" href="#">
+        <a href="#">新建文章</a>
+      </li>
+      <li class="dropdown-item" href="#">
+        <a href="#">编辑资料</a>
+      </li>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    return {
+      isOpen,
+      toggleOpen
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -6,19 +6,26 @@
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
     <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="" class="btn btn-outline-light my-2"> 你好 {{user.name}}</a></li>
+      <li class="list-inline-item">
+        <Dropdown :title="`你好 ${user.name}`"></Dropdown>
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import Dropdown from './Dropdown.vue'
 export interface UserProps {
   isLogin: boolean;
   name?: string;
   id?: number;
 }
 export default defineComponent({
+  name: 'GlobalHeader',
+  components: {
+    Dropdown
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
