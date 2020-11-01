30a9bd4c05becf4294be8aac2d1ed81a8228cee7
GarenWang <2270099149@qq.com>
2020-11-01 16:21:46 +0800

    feat: 添加ValidateInput组件支持抽象验证规则

--- src/App.vue
+++ src/App.vue
@@ -2,83 +2,39 @@
   <div class="container">
     <global-header :user="currentUser"></global-header>
     <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" v-model="emailRef.val" @blur="validateEmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div class="form-text">{{emailRef.message}}</div>
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules"></validate-input>
       </div>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-// const testData: ColumnProps[] = [
-//   {
-//     id: 1,
-//     title: 'test1的专栏',
-//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar:
-//       'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 2,
-//     title: 'test2的专栏',
-//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar:
-//       'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 3,
-//     title: 'test3的专栏',
-//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
-//   },
-//   {
-//     id: 4,
-//     title: 'test4的专栏',
-//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar:
-//       'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   }
-// ]
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'garen',
   id: 1
 }
-const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailRules: RulesProp = [
+  { type: 'required', message: '电子邮箱地址不能为空' },
+  { type: 'email', message: '请输入正确的电子邮箱' }
+]
 export default defineComponent({
   name: 'App',
   components: {
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-    const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be valid email'
-      }
-    }
     return {
       // list: testData,
       currentUser,
-      emailRef,
-      validateEmail
+      emailRules
     }
   }
 })
